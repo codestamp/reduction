fa41661ab6eda42b76b7c572e3a5163e711a07e6
codestamp <muneshchauhan@gmail.com>
2017-06-13 09:08:45 +0400

    added data directory

---
+++ build/CMakeFiles/reduc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda_compile.dir/source/cuda_compile_generated_reduc.cu.o"
+  "reduc.pdb"
+  "reduc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/reduc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
